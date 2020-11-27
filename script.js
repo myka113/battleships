7879a9c00c483ea380d402a8838e30369e590e8a
unknown <timberwolf1andonly@gmail.com>
2020-11-27 14:46:35 +0200

    2 commit

--- script.js
+++ script.js
@@ -1,120 +1,200 @@
 
-const cardContainer = document.getElementById('cardContainer')
-const footer = document.getElementById('footer')
-const timer = document.getElementById('timer')
+const grid = document.getElementById('grid')
+const chunk = document.getElementsByClassName('chunk')
 
-const card = document.getElementsByClassName('card')
+let chunks=[]
 
-function getInputValue(){
-  var inputVal = document.getElementById("myInput").value;
-  if(inputVal+1!=inputVal+2-1){
-    alert(`${inputVal} is not a number`);
-  }
-  else{
-    alert(`${inputVal} card pairs generated`);
-    footer.style.display='none'
-    generateCards(inputVal*1)
-    timer.innerHTML=`TIMER: ${inputVal*inputVal+5}`
+for(let y=0; y<=9; y++){
+  chunks.push([])
+  for(let x=0; x<=9; x++){
+    chunks[y].push(x)
+    grid.innerHTML+=`<div class="chunk" onclick="shoot(${y},${x});"></div>`
   }
 }
 
-let timerStarted=false
+console.log(chunks);
 
-let cardsArr=[]
+let ship=[]
 
-let biggestNum=1
+generateShip(4)
 
-function generateCards(inputVal){
-  console.log(inputVal);
+generateShip(3)
+generateShip(3)
 
-  for(let i=0; i<inputVal; i++){
-    cardsArr.push(biggestNum)
-    cardsArr.push(biggestNum)
-    biggestNum++
-  }
-  console.log(cardsArr);
-  shuffleArray(cardsArr)
-  console.log(cardsArr);
+generateShip(2)
+generateShip(2)
+generateShip(2)
+
+generateShip(1)
+generateShip(1)
+generateShip(1)
+generateShip(1)
 
 
-  for(let i=0; i<inputVal*2; i++){
-    cardContainer.innerHTML+=`<div class="card" onclick="flip(${i});"></div>`
-  }
+let checkCount=0
+
+while(touchCheck()){
+  ship=[]
+
+  generateShip(4)
+  
+  generateShip(3)
+  generateShip(3)
+  
+  generateShip(2)
+  generateShip(2)
+  generateShip(2)
+  
+  generateShip(1)
+  generateShip(1)
+  generateShip(1)
+  generateShip(1)
+
+  checkCount++
 }
 
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array
-}
+console.log(`-----loop count-----`);
+console.log(checkCount);
 
-let cardPressed=false
-let cardPressedNum=false
-let whichCard2=false
-
-function flip(whichCard) {
-  if(!timerStarted){
-    startTimer(cardsArr.length/2)
-    timerStarted=true
-  }
-  whichCard2=whichCard
-  if(!cardPressed){
-    cardPressed=cardsArr[whichCard]
-    cardPressedNum=whichCard
-    console.log(cardPressedNum);
-    card[whichCard].innerHTML=`${cardsArr[whichCard]}`
-  }
-  else{
-    card[whichCard].innerHTML=`${cardsArr[whichCard]}`
-    if(cardPressed===cardsArr[whichCard]){
-      console.log('true');
-      card[cardPressedNum].classList.add("noClick");
-      card[whichCard2].classList.add("noClick");
-      card[cardPressedNum].onclick=null
-      card[whichCard2].onclick=null
+function touchCheck(){
+  let trigger=false
+  for(let i=0; i<ship.length; i++){
+    for(let j=0; j<ship[i].length; j++){
+      
+      for(let i2=0; i2<ship.length; i2++){
+        for(let j2=0; j2<ship[i2].length; j2++){
+          if(ship[i2]!==ship[i]){
+            if((ship[i2][j2][0]===ship[i][j][0]&&ship[i2][j2][1]===ship[i][j][1])||isItTouching(i, j, i2, j2)){
+              trigger=true
+            }
+          }
+          
+        }
+      }
+      
     }
-    else{
-      console.log('false');
-      curtain.style.display='block'
-      setTimeout(flipDown, 2000)
-    }
-    cardPressed=false
   }
-}
-
-function flipDown(){
-  curtain.style.display='none'
-  card[cardPressedNum].innerHTML=''
-  card[whichCard2].innerHTML=''
-  cardPressedNum=false
-}
-
-let time=0
-
-function startTimer(inputVal){
-  time=inputVal*inputVal+5
-  timer.innerHTML=`TIMER: ${time}`
-  count()
-}
 
 
-function count(){
-  setInterval(subtractASecond, 1000)
-}
-
-function subtractASecond(){
-  if(time){
-    console.log(time);
-    time--
-    timer.innerHTML=`TIMER: ${time}`
+  if(trigger){
+    return true
   }
   else{
-    timer.innerHTML=`OUT OF TIME !!!`
-    timer.style.fontSize='100px'
-    curtain.style.display='block'
+    return false
   }
 }
 
 
+function isItTouching(i, j, i2, j2){
+  if(ship[i2][j2][0]+1===ship[i][j][0]&&ship[i2][j2][1]===ship[i][j][1]){
+    return true
+  }
+  if(ship[i2][j2][0]===ship[i][j][0]&&ship[i2][j2][1]+1===ship[i][j][1]){
+    return true
+  }
+}
+
+
+
+
+
+console.log('--------------------');
+console.log(ship);
+
+function generateShip(num){
+  let shipCordinates=[]
+  let xory=0
+  if(Math.floor(Math.random() * 2)){
+    xory='x'
+  }else{
+    xory='y'
+  }
+  let origin=[]
+  let originy
+  let originx
+  if(xory==='y'){
+    originy=Math.floor(Math.random() * (11-num))
+  }
+  else{
+    originy=Math.floor(Math.random() * 10)
+  }
+  if(xory==='x'){
+    originx=Math.floor(Math.random() * (11-num))
+  }
+  else{
+    originx=Math.floor(Math.random() * 10)
+  }
+
+  
+
+
+  origin.push(originy)
+  origin.push(originx)
+
+  shipCordinates.push(origin)
+  if(num!==1){
+    let neighbours=[]
+    let county=originy
+    let countx=originx
+    for(let i=1; i<num; i++){
+
+      if(xory==='y'){
+        if(county<9){
+          county++
+          neighbours.push([county, originx])
+        }
+      }
+
+      if(xory==='x'){
+        if(countx<9){
+          countx++
+          neighbours.push([originy, countx])
+        }
+      }
+
+      
+    }
+
+    for(let i=0; i<neighbours.length; i++){
+      shipCordinates.push(neighbours[i])
+    }
+  }
+
+
+  ship.push(shipCordinates)
+}
+
+let shipParts=20
+let shots=0
+
+
+const shotsFired = document.getElementById('shotsFired')
+const curtain = document.getElementById('curtain')
+const winScren = document.getElementById('winScren')
+
+function shoot(y,x){
+  let whichChunk=(y*10)+x
+  let trigger=false
+  for(let i=0; i<ship.length; i++){
+    for(let j=0; j<ship[i].length; j++){
+
+      if(y===ship[i][j][0]&&x===ship[i][j][1]){
+        trigger=true
+      }
+
+    }
+  }
+  if(trigger){
+    chunk[whichChunk].classList.add("red");
+    shipParts--
+  }else{
+    chunk[whichChunk].classList.add("blue");
+    shots++
+    shotsFired.innerHTML=`shots fired: ${shots}`
+  }
+  chunk[whichChunk].onclick=null
+  if(shipParts===0){
+    curtain.style.display="block"
+    winScren.innerHTML=`YOU WIN !!! (shots fired: ${shots})`
+  }
+}
